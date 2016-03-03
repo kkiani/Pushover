dce154b28eeadbe707549075f7b321555309867b
kiarash <hi@kiarash.info>
2016-03-03 11:38:18 +0330

    [add] Message Priority Enum class

--- libpushover.py
+++ libpushover.py
@@ -1,5 +1,11 @@
 import http.client, urllib
+from enum import Enum
 
+class MessagePriority(Enum):
+    Lowest_Priority = -2
+    Low_Priority = -1
+    Normal_Priority = 0
+    High_Priority = 1
 
 class PushService():
 
@@ -7,7 +13,7 @@ class PushService():
         self.token = token
         self.user_id = user_id
 
-    def pushMessage(self, title, message, url, url_title, priority=0):
+    def pushMessage(self, title, message, url, url_title, priority=MessagePriority.Normal_Priority):
         conn = http.client.HTTPSConnection("api.pushover.net:443")
         conn.request("POST", "/1/messages.json",
         urllib.parse.urlencode({
@@ -18,6 +24,6 @@ class PushService():
             "message": message,
             "url":url,
             "url_title":url_title,
-            "priority":priority,
+            "priority":priority.value,
         }), { "Content-type": "application/x-www-form-urlencoded" })
         conn.getresponse()
